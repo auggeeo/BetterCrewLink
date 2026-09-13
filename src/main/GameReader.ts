cf250dd71d3452f5a5a9a94477e67324561693d5
August <cooptheloop8@gmail.com>
2026-09-13 12:33:14 -0500

    fix linting i think!

--- src/main/GameReader.ts
+++ src/main/GameReader.ts
@@ -97,7 +97,9 @@ export default class GameReader {
 					this.pid = processOpen.th32ProcessID;
 					this.amongUs = openProcess(processOpen.th32ProcessID);
 					this.gameAssembly = findModule('GameAssembly.dll', this.amongUs.th32ProcessID);
-					this.gamePath = this.is_linux ? `/proc/${this.pid}/cwd${getProcessPath(this.amongUs.handle)}` : getProcessPath(this.amongUs.handle);
+					this.gamePath = this.is_linux
+						? `/proc/${this.pid}/cwd${getProcessPath(this.amongUs.handle)}`
+						: getProcessPath(this.amongUs.handle);
 					this.loadedMod = this.getInstalledMods(this.gamePath);
 					await this.initializeoffsets();
 					this.sendIPC(IpcRendererMessages.NOTIFY_GAME_OPENED, true);
